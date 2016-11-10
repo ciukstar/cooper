b9abc4fb4dcccabe2bb58f56093b64ab2b8a3c19
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-10 21:01:24 +0300

    Add more entities

---
+++ src/main/java/edu/ciukstar/cooper/domain/Dispencer.java
@@ -0,0 +1,56 @@
+package edu.ciukstar.cooper.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author sergiu
+ */
+@Entity
+@Table(name = "dispencers")
+public class Dispencer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Dispencer)) {
+            return false;
+        }
+        Dispencer other = (Dispencer) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "edu.ciukstar.cooper.domain.Dispencer[ id=" + id + " ]";
+    }
+    
+}
