5bac56cb3b3ccccd0d1f8c92fd4c2da7478404f3
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-11 20:47:06 +0300

    Implement equality protocol for FullName

--- src/main/java/edu/ciukstar/cooper/domain/FullName.java
+++ src/main/java/edu/ciukstar/cooper/domain/FullName.java
@@ -1,6 +1,7 @@
 package edu.ciukstar.cooper.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import org.hibernate.validator.constraints.NotBlank;
@@ -11,6 +12,39 @@ import org.hibernate.validator.constraints.NotBlank;
  */
 @Embeddable
 public class FullName implements Serializable {
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.surname);
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + Objects.hashCode(this.patronymic);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FullName other = (FullName) obj;
+        if (!Objects.equals(this.surname, other.surname)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.patronymic, other.patronymic)) {
+            return false;
+        }
+        return true;
+    }
 
     public String getSurname() {
         return surname;
--- src/main/java/edu/ciukstar/cooper/domain/Order.java
+++ src/main/java/edu/ciukstar/cooper/domain/Order.java
@@ -12,7 +12,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.NotBlank;
 
 /**
