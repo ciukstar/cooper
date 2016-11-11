c4ec06916b2cd21d7c68e2d51262a06933d7c182
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-11 20:49:11 +0300

    Implement equals protocol for User

--- src/main/java/edu/ciukstar/cooper/domain/User.java
+++ src/main/java/edu/ciukstar/cooper/domain/User.java
@@ -1,6 +1,7 @@
 package edu.ciukstar.cooper.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,6 +18,39 @@ import org.hibernate.validator.constraints.NotBlank;
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.username);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        return true;
+    }
+
+    public FullName getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(FullName fullName) {
+        this.fullName = fullName;
+    }
 
     public Long getId() {
         return id;
@@ -36,14 +70,6 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public FullName getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(FullName fullName) {
-        this.fullName = fullName;
     }
 
     public String getSurname() {
@@ -77,21 +103,21 @@ public class User implements Serializable {
     public void setPhoto(byte[] photo) {
         this.photo = photo;
     }
-    
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     @Embedded
     private FullName fullName;
-    
+
     @NotBlank(message = "{Username_may_not_be_blank}")
     private String username;
-    
+
     @NotBlank(message = "{Password_may_not_be_blank}")
     private String password;
-    
+
     @Lob
     private byte[] photo;
 }
