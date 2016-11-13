fffc6f52ea8cd5ae0a0aec3508945a8c6b730a7a
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-13 16:16:41 +0300

    Add property User.email. Add property User.phone

--- src/main/java/edu/ciukstar/cooper/domain/User.java
+++ src/main/java/edu/ciukstar/cooper/domain/User.java
@@ -2,6 +2,7 @@ package edu.ciukstar.cooper.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 /**
@@ -18,27 +21,35 @@ import org.hibernate.validator.constraints.NotBlank;
  * @author sergiu
  */
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"username"})})
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+
     @NotBlank(message = "{Username_may_not_be_blank}")
     private String username;
-    
+
     @NotBlank(message = "{Password_may_not_be_blank}")
     private String password;
-    
+
     @Embedded
     private FullName fullName;
-    
+
     @ManyToOne
     @JoinColumn(name = "status", referencedColumnName = "id", nullable = false)
     private Status status;
+
+    @Email(message = "{Invalid_email_address}")
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "phone")
+    private String phone;
     
     @Lob
     private byte[] photo;
@@ -138,6 +149,22 @@ public class User implements Serializable {
 
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
 }
