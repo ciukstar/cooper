62fb0f42c2815d37439f832919f57015ff106b73
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-14 22:56:30 +0300

    Factor out js

---
+++ src/main/webapp/resources/js/states.js
@@ -0,0 +1,7 @@
+
+$('#statesForm\\:addNewStatusMenuitem').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('#statesForm\\:addStatusCommandButtonHidden').click();
+});
+
