9648ad3262245dfb4f711a54f8d8b24ff41c694a
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-18 23:34:09 +0300

    Implement 'Create a new user with status Active'

--- src/main/java/edu/ciukstar/cooper/domain/UserBuilder.java
+++ src/main/java/edu/ciukstar/cooper/domain/UserBuilder.java
@@ -10,7 +10,14 @@ import javax.inject.Named;
 @Named
 @Dependent
 public class UserBuilder {
-    public User newUser() {
-        return new User();
+    
+    public User newUser(Graph graph) {
+        return newUser(graph.getStartNode());
+    }
+    
+    public User newUser(Status status) {
+        User user = new User();
+        user.setStatus(status);
+        return user;
     }
 }
--- src/main/java/edu/ciukstar/cooper/repo/GraphRepo.java
+++ src/main/java/edu/ciukstar/cooper/repo/GraphRepo.java
@@ -1,13 +1,19 @@
 package edu.ciukstar.cooper.repo;
 
 import edu.ciukstar.cooper.domain.Graph;
+import edu.ciukstar.cooper.domain.Graph_;
 import java.util.List;
+import java.util.Optional;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -21,6 +27,18 @@ public class GraphRepo extends AbstractRepo<Graph> {
     private EntityManager em;
     @Inject
     private Event<List<Graph>> event;
+
+    public Optional<Graph> findByCode(String code) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Graph> cq = cb.createQuery(Graph.class);
+        Root<Graph> graph = cq.from(Graph.class);
+        cq.select(graph).where(cb.equal(graph.get(Graph_.code), code));
+        try {
+            return Optional.of(getEntityManager().createQuery(cq).getSingleResult());
+        } catch (NoResultException ex) {
+            return Optional.empty();
+        }
+    }
 
     @Override
     public List<Graph> findAll() {
