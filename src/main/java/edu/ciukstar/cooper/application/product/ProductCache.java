eb712283e54e876e74686386c51dd547c39fe064
Sergiu Starciuc <ciukstar@yahoo.com>
2016-11-23 20:17:48 +0300

    Implement 'Edit Product'

--- src/main/java/edu/ciukstar/cooper/application/product/ProductCache.java
+++ src/main/java/edu/ciukstar/cooper/application/product/ProductCache.java
@@ -1,11 +1,15 @@
 package edu.ciukstar.cooper.application.product;
 
 import edu.ciukstar.cooper.application.CrudCache;
+import edu.ciukstar.cooper.application.Refresher;
 import edu.ciukstar.cooper.domain.Product;
 import edu.ciukstar.cooper.repo.CrudOperation;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import org.primefaces.event.FileUploadEvent;
 
 /**
@@ -16,16 +20,22 @@ import org.primefaces.event.FileUploadEvent;
 @SessionScoped
 public class ProductCache extends CrudCache<Product> implements Serializable {
 
+    @Inject
+    private Refresher refresher;
     private CrudOperation<Product> op;
     private Product entity;
+
+    void refresh(@Observes List<Product> source) {
+        this.entity = refresher.match(entity, source).orElse(null);
+    }
 
     public void uploadImage(FileUploadEvent e) {
         entity.setImage(e.getFile().getContents());
     }
-    
+
     @Override
     protected void setCrudOperation(CrudOperation<Product> op) {
-        this.op = op;        
+        this.op = op;
     }
 
     @Override
@@ -42,5 +52,5 @@ public class ProductCache extends CrudCache<Product> implements Serializable {
     public Product getEntity() {
         return entity;
     }
-    
+
 }
