581b2df51d39ebf46ccda2cf6e76e5034ba55f21
Sergiu Starciuc <ciukstar@yahoo.com>
2017-01-08 23:23:00 +0300

    Track user sessions

--- src/main/java/edu/ciukstar/cooper/application/admin/UserSession.java
+++ src/main/java/edu/ciukstar/cooper/application/admin/UserSession.java
@@ -2,6 +2,7 @@ package edu.ciukstar.cooper.application.admin;
 
 import edu.ciukstar.cooper.domain.User;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -14,10 +15,20 @@ import javax.inject.Named;
 public class UserSession implements Serializable {
 
     private User user;
+    private LocalDateTime start;
 
     public UserSession setupUserSession(User user) {
         this.user = user;
+        this.start = LocalDateTime.now();
         return this;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public User getUser() {
+        return user;
     }
     
 }
