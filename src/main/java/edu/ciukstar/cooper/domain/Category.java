51e539a5f39e2ac6243ca08150dec641c275670c
Sergiu Starciuc <ciukstar@yahoo.com>
2017-01-29 23:12:19 +0300

    Add property Category.subcategory

--- src/main/java/edu/ciukstar/cooper/domain/Category.java
+++ src/main/java/edu/ciukstar/cooper/domain/Category.java
@@ -1,12 +1,20 @@
 package edu.ciukstar.cooper.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import org.hibernate.validator.constraints.NotBlank;
 
 /**
@@ -30,6 +38,32 @@ public class Category implements Persistable<Long> {
     @Column(name = "DESCRIPTION")
     private String description;
 
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "SUBCATEGORIES",
+            joinColumns = {
+                @JoinColumn(name = "CATEGORY", nullable = false, referencedColumnName = "ID")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "SUBCATEGORY", nullable = false, referencedColumnName = "ID")},
+            uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"CATEGORY", "SUBCATEGORY"})})
+    private List<Category> subcategories;
+
+    public Category() {
+        this.subcategories = new ArrayList<>();
+    }
+
+    public List<Category> getSubcategories() {
+        return new ArrayList<>(subcategories);
+    }
+    
+    public void addSubcategory(Category subcategory) {
+        this.subcategories.add(subcategory);
+    }
+    
+    public void removeSubcategory(Category subcategory) {
+        this.subcategories.remove(subcategory);
+    }
+    
     @Override
     public int hashCode() {
         int hash = 5;
