0e7bb971f9ac7b5b574f5cf15d1407c7ca033fd9
Sergiu Starciuc <ciukstar@yahoo.com>
2017-02-03 23:32:37 +0300

    Add cart header

--- src/main/java/edu/ciukstar/cooper/application/cart/Cart.java
+++ src/main/java/edu/ciukstar/cooper/application/cart/Cart.java
@@ -1,11 +1,16 @@
 package edu.ciukstar.cooper.application.cart;
 
 import edu.ciukstar.cooper.domain.Article;
+import edu.ciukstar.cooper.domain.Currency;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
 
 /**
  *
@@ -19,6 +24,16 @@ public class Cart implements Serializable {
 
     public Cart() {
         this.articles = new ArrayList<>();
+    }
+
+    public Map<Currency, BigDecimal> getAmountByCurrency() {
+        return articles.stream().collect(
+                groupingBy(Article::getCurrency, 
+                        reducing(BigDecimal.ZERO, Article::getPrice, BigDecimal::add)));
+    }
+    
+    public BigDecimal getAmount() {
+        return articles.stream().map(Article::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     public boolean contains(Article article) {
@@ -39,5 +54,9 @@ public class Cart implements Serializable {
     
     public String getSize() {
         return String.valueOf(articles.size());
+    }
+    
+    public boolean isEmpty() {
+        return this.articles.isEmpty();
     }
 }
